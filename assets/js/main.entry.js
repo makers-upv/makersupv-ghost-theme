ac584ed1d8675adfd91adbcf7cd8b511d05c2f31
Carlos Precioso <kalprestito@gmail.com>
2018-03-07 17:50:32 +0100

    Reorganize source files
    Remove compiled files from repository

--- assets/js/main.entry.js
+++ assets/js/main.entry.js
@@ -5,32 +5,32 @@ var swal = require("sweetalert")
 var loaded = false
 function lazyload() {
   if (!loaded) {
-    loaded = u("img.lazy").each(function(node) {
+    loaded = u("img.lazy").each(function (node) {
       u(node).attr("src", u(node).attr("data-src"))
     })
   }
 }
 u([window]).on("scroll", lazyload)
 var body = u("body")
-u([window]).on("scroll", function(e) {
+u([window]).on("scroll", function (e) {
   u("nav").toggleClass("active", body.size().top < -10)
 })
 
 // Filter the different parts with the buttons
 function reloadParts() {
   var checked = []
-  u(".category input:checked").each(function(data) {
+  u(".category input:checked").each(function (data) {
     checked.push(u(data).attr("data-type"))
   })
 
-  u("article").each(function(article) {
+  u("article").each(function (article) {
     u(article).addClass("hidden")
 
     if (checked.length === 0) {
       u(article).removeClass("hidden")
     }
 
-    checked.forEach(function(one) {
+    checked.forEach(function (one) {
       if (u(article).hasClass("tag-" + one)) {
         lazyload()
         u(article).removeClass("hidden")
@@ -43,14 +43,14 @@ u(".category input").on("change", reloadParts)
 reloadParts()
 
 u("#contacto form").ajax(
-  function(err, data) {
+  function (err, data) {
     console.log(err, data)
     if (err) {
       swal(
         "Oops!",
         "Algo ha fallado, por favor contáctanos en contacto" +
-          "@" +
-          "makersupv.com",
+        "@" +
+        "makersupv.com",
         "error"
       )
     } else {
@@ -64,7 +64,7 @@ u("#contacto form").ajax(
         .reset()
     }
   },
-  function() {
+  function () {
     u('[name="_cc"]').first().value =
       "public" + "francisco" + "@" + "hotmail.com"
   }
